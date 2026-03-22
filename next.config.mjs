77ced59fc7e3fd63c97ba9802890dff8b4788b8c
Tharindu Dahampriya Mendis <tharindumendis100@outlook.com>
2026-03-22 13:55:36 +0530

    Add project configuration files

---
+++ next.config.mjs
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      { protocol: 'https', hostname: 'lh3.googleusercontent.com' },
+      { protocol: 'https', hostname: '*.googleusercontent.com' },
+      { protocol: 'https', hostname: 'firebasestorage.googleapis.com' },
+    ],
+  },
+};
+
+export default nextConfig;
